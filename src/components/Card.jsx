ab24f997bf148731910b1640cda9200d6b156aa1
Prasad Hp <prasadhp@outlook.in>
2024-06-14 13:20:16 +0530

    List of places in homepage fixed for different screen sizes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,26 @@
-import React from 'react'
+import React from 'react';
 
 function Card(props) {
   return (
-    <div className='w-11/12 sm:w-auto'>
-        <div className='relative '>
-            <img src={props.img} alt="photo" className='rounded-lg sm:w-68 sm:h-68 w-full ' />
-            <img src="" alt="" className='absolute right-3 top-2'/>
-        </div>
-        <div className='sm:w-68 w-10/12'>
-          <div className='flex justify-between'>
-            <p className='font-semibold'>{props.location}</p>
-            <p className='font-semibold'>&#9733;{" "}{props.star}</p>
+    <div className='w-full'>
+      <div className="flex justify-center">
+        <div className='w-11/12 flex flex-col items-center align-middle justify-center'>
+          <div className='relative'>
+            <img src={props.img} alt="photo" className='rounded-lg 2xl:h-60 xl:h-56 md:h-48 sm:h-60 2xl:w-60 xl:w-56 md:w-48 sm:w-60' />
+            <img src="" alt="" className='absolute right-3 top-2' />
           </div>
-            <p className='text-gray-500'>{props.description}</p>
+          <div className='sm:w-68 w-11/12'>
+            <div className='flex justify-between'>
+              <p className='font-semibold'>{props.location}</p>
+              <p className='font-semibold'>&#9733;{" "}{props.star}</p>
+            </div>
+            <p className='text-gray-500'>{props.title}</p>
             <p className='font-normal'>{props.price}t</p>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
